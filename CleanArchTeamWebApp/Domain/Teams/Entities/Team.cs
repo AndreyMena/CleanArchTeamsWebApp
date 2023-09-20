9266a39edccb56491bc41f6bb477949dc9251cf9
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-09-20 14:31:24 -0600

    Add team entity class

---
+++ CleanArchTeamWebApp/Domain/Teams/Entities/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Core.Entities;
+
+namespace Domain.Teams.Entities
+{
+    public class Team : AggregateRoot
+    {
+        public string Name { get; private set; }
+        public string Region { get; private set; }
+        public int Foundation { get; private set; }
+
+        public Team(string name, string region, int foundation)
+        {
+            Name = name;
+            Region = region;
+            Foundation = foundation;
+        }
+    }
+}
