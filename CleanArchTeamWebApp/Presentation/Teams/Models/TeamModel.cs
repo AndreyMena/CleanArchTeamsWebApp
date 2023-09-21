b6d3adfd79fc86aacb7c6c76259849740167d181
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-09-20 21:11:54 -0600

    Fix mudblazor components

---
+++ CleanArchTeamWebApp/Presentation/Teams/Models/TeamModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Teams.Models
+{
+    public class TeamModel
+    {
+        public string? Name { get; set; }
+        public string? Region { get; set; }
+        public string? Foundation { get; set; }
+    }
+}
