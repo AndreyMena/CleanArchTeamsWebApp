d6e860b4c7d095bca9b87da4011936ee2da7849b
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-10-01 15:52:32 -0600

    Add footbal team repository and interface

---
+++ CleanArchTeamWebApp/Domain/Teams/Repositories/IFootballTeamRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Core.Repositories;
+using Domain.Teams.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Teams.Repositories
+{
+    public interface IFootballTeamRepository : IRepository<FootballTeam>, ITeamRepository
+    {
+        Task<IFootballTeamRepository?> GetStadiumName(int id);
+    }
+}
---
+++ CleanArchTeamWebApp/Infrastructure/Teams/Repositories/FootballTeamRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Teams.Repositories;
+using Infrastructure.Teams.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Teams.Repositories
+{
+    internal class FootballTeamRepository : TeamRepository, IFootballTeamRepository
+    {
+        public FootballTeamRepository(TeamDbContext unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public Task<IFootballTeamRepository?> GetStadiumName(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
