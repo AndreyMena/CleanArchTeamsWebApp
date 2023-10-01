cfde1bc2603b43ed9a498e6b115011893a2aef51
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-10-01 15:51:34 -0600

    Add table for football team

---
+++ CleanArchTeamWebApp/DatabaseTeamsWebApp/Tables/FootballTeam.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FootballTeam]
+(
+	[Id] INT NOT NULL,
+	[StadiumName] VARCHAR(30) NULL,
+	CONSTRAINT [PK_FootballTeam] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_FootbalTeam_Team] FOREIGN KEY 
+    ([Id]) REFERENCES [Team]([Id])
+)
