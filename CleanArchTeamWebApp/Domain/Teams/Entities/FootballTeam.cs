cd7222fe3f62e20b2329ab1fc93f0e359bdb5f74
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-10-01 15:52:18 -0600

    Add entity of fottabll team and mapping

---
+++ CleanArchTeamWebApp/Domain/Teams/Entities/FootballTeam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.Teams.Entities
+{
+    public class FootballTeam : Team
+    {
+        public string StadiumName { get; private set; }
+
+        public FootballTeam(string name, string region, int foundation, string stadiumName) : base(name, region, foundation)
+        {
+            StadiumName = stadiumName;
+        }
+    }
+}
---
+++ CleanArchTeamWebApp/Infrastructure/Teams/EntityMappings/FootbalTeamMap.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Teams.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Teams.EntityMappings
+{
+    public class FootbalTeamMap : IEntityTypeConfiguration<FootballTeam>
+    {
+        public void Configure(EntityTypeBuilder<FootballTeam> builder)
+        {
+            builder.ToTable("FootballTeam");
+
+            builder.Property(r => r.StadiumName);
+        }
+    }
+}
